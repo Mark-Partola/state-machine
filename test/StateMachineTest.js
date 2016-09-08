7a96a56dc91fe45647df56b2d167fb45798e0764
Mark Partola <partolamark@yandex.ru>
2016-09-09 01:04:29 +0300

    Окружение для тестов

--- test/StateMachineTest.js
+++ test/StateMachineTest.js
@@ -1,8 +1,10 @@
-import StateMachine from './../src/StateMachine'
+import StateMachine from 'C:/Users/m.partola/projects/statemachine/src/StateMachine';
 
 describe('Тестирование машины состояний', () => {
 
     it('Должен создаваться экземпляр класса', () => {
+        console.log(typeof StateMachine);
+
         assert(new StateMachine() instanceof StateMachine);
     });
 });
